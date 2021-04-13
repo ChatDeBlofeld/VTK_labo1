20f9bb1b0701a96cceb10ba1d1b31c00e2d92696
Gildas <gildas.houlmann@gmail.com>
2021-04-13 12:14:00 +0200

    added eyes and lost my mind on transformations

--- snowman.py
+++ snowman.py
@@ -31,6 +31,18 @@ head.SetRadius( 1.5 )
 head.SetThetaResolution(15)
 head.SetPhiResolution(15)
 
+leftEye = vtk.vtkSphereSource()
+leftEye.SetCenter(0.5, 3.5, 1.3)
+leftEye.SetRadius( 0.2 )
+leftEye.SetThetaResolution(15)
+leftEye.SetPhiResolution(15)
+
+rightEye = vtk.vtkSphereSource()
+rightEye.SetCenter(-0.5, 3.5, 1.3)
+rightEye.SetRadius( 0.2 )
+rightEye.SetThetaResolution(15)
+rightEye.SetPhiResolution(15)
+
 nose = vtk.vtkConeSource()
 nose.SetCenter(4, 0, 0)
 nose.SetDirection(0, -1, 0)
@@ -55,6 +67,12 @@ bodyMapper.SetInputConnection( body.GetOutputPort() )
 noseMapper = vtk.vtkPolyDataMapper()
 noseMapper.SetInputConnection( nose.GetOutputPort() )
 
+leftEyeMapper = vtk.vtkPolyDataMapper()
+leftEyeMapper.SetInputConnection( leftEye.GetOutputPort() )
+
+rightEyeMapper = vtk.vtkPolyDataMapper()
+rightEyeMapper.SetInputConnection( rightEye.GetOutputPort() )
+
 #
 # Create an actor to represent the cone. The actor orchestrates rendering of
 # the mapper's graphics primitives. An actor also refers to properties via a
@@ -69,6 +87,17 @@ bodyActor.SetMapper( bodyMapper )
 
 noseActor = vtk.vtkActor()
 noseActor.SetMapper( noseMapper )
+noseActor.GetProperty().SetColor(255, 165, 0)
+
+leftEyeActor = vtk.vtkActor()
+leftEyeActor.SetMapper( leftEyeMapper )
+leftEyeActor.GetProperty().SetColor(0, 0, 0)
+leftEyeActor.SetVisibility(False)
+
+rightEyeActor = vtk.vtkActor()
+rightEyeActor.SetMapper( rightEyeMapper )
+rightEyeActor.GetProperty().SetColor(0, 0, 0)
+rightEyeActor.SetVisibility(False)
 
 #
 # Create the Renderer and assign actors to it. A renderer is like a
@@ -80,6 +109,8 @@ ren1= vtk.vtkRenderer()
 ren1.AddActor( headActor )
 ren1.AddActor( bodyActor )
 ren1.AddActor(noseActor)
+ren1.AddActor(leftEyeActor)
+ren1.AddActor(rightEyeActor)
 ren1.SetBackground( 0.1, 0.2, 0.4 )
 ren1.GetActiveCamera().SetFocalPoint(0,0,0)
 ren1.GetActiveCamera().SetPosition(0,0,22)
@@ -94,58 +125,70 @@ renWin.AddRenderer( ren1 )
 renWin.SetSize( 500, 500 )
 
 
-
-# 
+#
+# rotate head to over the body
 # 0:00 - 0:03
 #
 for i in range (0, 90):
     time.sleep(0.03)
-    renWin.Render()
     headActor.RotateZ(-1)
+    renWin.Render()
 
-# 
+#
+# lower the head into the body
 # 0:03 - 0:06
 #
 lower = vtk.vtkTransform()
 headActor.SetUserTransform(lower)
-for i in range (0, 8):
-    time.sleep(0.06)
-    lower.Translate(0, -0.1, 0)
+for i in range (0, 80):
+    time.sleep(0.01)
+    lower.Translate(0, -0.01, 0)
     renWin.Render()
 
-# 
+#
+# rotate the nose in front of the body
 # 0:06 - 0:09
 #
 for i in range (0, 90):
     time.sleep(0.03)
-    renWin.Render()
     noseActor.RotateY(-1)
+    renWin.Render()
 
-# 
+#
+# bring the nose closer to the body
 # 0:09 - 0:11
 #
 noseTranslate = vtk.vtkTransform()
 noseActor.SetUserTransform(noseTranslate)
-for i in range (0, 7):
-    time.sleep(0.03)
-    noseTranslate.Translate(0, 0, -0.1)
+for i in range (0, 80):
+    time.sleep(0.01)
+    noseTranslate.Translate(0, 0, -0.01)
     renWin.Render()
 
-# 
+#
+# rotate the nose into the head
 # 0:11 - 0:14
 #
 for i in range (0, 90):
     time.sleep(0.03)
-    renWin.Render()
     noseActor.RotateZ(1)
+    renWin.Render()
 
-# 
+#
+# bring the nose at the surface of the face
 # 0:14 - 0:16
 #
+for i in range (0, 175):
+    time.sleep(0.01)
+    noseTranslate.Translate(0, 0, 0.01)
+    renWin.Render()
 
-#
+
+# 
 # Adding eyes - 0:16
 #
+leftEyeActor.SetVisibility(True)
+rightEyeActor.SetVisibility(True)
 
 #
 # 0:16 - 0:22
